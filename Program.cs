e5a1ba5ee56f2c609a42444e307c145c4820d354
Lyam sauvage <140600522+Lyam02@users.noreply.github.com>
2023-07-27 09:14:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace compteur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msgConsole = "je suis un compteur simple a utiliser!";
+
+            Console.WriteLine(msgConsole);
+
+            Console.WriteLine("Il faut que tu entre le premier nombre ;)");
+            string input = Console.ReadLine();
+            int PremierNombre = int.Parse(input);
+
+            Console.WriteLine("Entre maintenant le second nombre");
+            int SecondNombre = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("les chiffres sont:");
+
+            for (int i = PremierNombre; i <= SecondNombre; i = i +1)
+            {
+                Console.WriteLine(i);
+
+            }
+
+        }   
+
+    }
+}
